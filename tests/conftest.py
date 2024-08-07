0678c833c91bccf557432e708fe00e00392e1d7c
pavlentiod <levap.spb@gmail.com>
2024-08-07 15:07:13 +0300

    Add confest for pytest with db connection

---
+++ tests/conftest.py
@@ -0,0 +1,42 @@
+from typing import AsyncGenerator
+
+import pytest
+from sqlalchemy.ext.asyncio import AsyncConnection, AsyncTransaction, create_async_engine, AsyncSession
+
+from config import settings
+
+engine = create_async_engine(settings.db.url)
+pytestmark = pytest.mark.anyio
+
+
+@pytest.fixture(scope="session")
+def anyio_backend():
+    return "asyncio"
+
+
+@pytest.fixture(scope="session")
+async def connection(anyio_backend) -> AsyncGenerator[AsyncConnection, None]:
+    async with engine.connect() as connection:
+        yield connection
+
+
+@pytest.fixture
+async def transaction(
+        connection: AsyncConnection,
+) -> AsyncGenerator[AsyncTransaction, None]:
+    async with connection.begin() as transaction:
+        yield transaction
+
+
+@pytest.fixture
+async def session(
+        connection: AsyncConnection, transaction: AsyncTransaction
+) -> AsyncGenerator[AsyncSession, None]:
+    async_session = AsyncSession(
+        bind=connection,
+        join_transaction_mode="create_savepoint",
+    )
+
+    yield async_session
+
+    await transaction.rollback()
